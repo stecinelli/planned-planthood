1556a0d08b3ac49fab4a4cc0aaa6d25289c41f9b
stecinelli <stephaniecinelli@gmail.com>
2024-01-03 14:05:02 +0100

    hero

---
+++ planthood/src/Components/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Style/Hero.css";
+import seedling from "../assets/seedling.svg";
+import dig from "../assets/dig.svg";
+import logo from "../assets/Logo.svg";
+
+export const Hero = () => {
+  return (
+    <div className="box">
+      <div className="hero">
+        <div className="overlap-group">
+          <img className="seedling" alt="Seedling" src={seedling} />
+        </div>
+        <div className="overlap">
+          <img className="dig" alt="Dig" src={dig} />
+        </div>
+        <div className="logo-wrapper">
+          <img className="logo" alt="Logo" src={logo} />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ planthood/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import "../Style/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="box">
+      <div className="nav-bg">
+        <div className="nav">
+          <div className="text-wrapper">PLANER</div>
+          <div className="text-wrapper">HISTORIK</div>
+          <div className="text-wrapper">TIPS</div>
+          <div className="div">LOGGA IN</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
